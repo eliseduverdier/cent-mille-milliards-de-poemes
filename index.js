3c0bd38ca3dd1185be7b8bbe91d826f8b77201d1
Elise Duverdier <e.duverdier@wakeonweb.com>
2021-08-19 15:55:26 +0200

    feat: add compiled

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const loader = require("@assemblyscript/loader/umd");
 const imports = { /* imports go here */ };
 const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
 module.exports = wasmModule.exports;
-const poemModule = require("assembly/poem-chooser");
-poemModule.displayPoem();
+// const poemModule = require(__dirname + "/assembly/poem-chooser");
+// poemModule.displayPoem();
